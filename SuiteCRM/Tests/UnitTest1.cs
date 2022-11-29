4d7e0a636ba272c3c9dcf77a81cc948df6824177
Sreekanth8639 <102298707+Sreekanth8639@users.noreply.github.com>
2022-11-29 15:17:35 +0530

    Update UnitTest1.cs
    Change utiles

--- SuiteCRM/Tests/UnitTest1.cs
+++ SuiteCRM/Tests/UnitTest1.cs
@@ -23,34 +23,6 @@ namespace SuiteCRM.Tests
             loginClassPageOjbects.login.Click();
            
 
-            //Creat account Check
-          /*  AccountsClass accountsClassPageObjects = new AccountsClass(driver);
-            WebDriverWait wait1 = new WebDriverWait(driver, TimeSpan.FromSeconds(200));
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//a[normalize-space()='Accounts']")));
-            accountsClassPageObjects.counts.Click();
-            WebDriverWait wait2 = new WebDriverWait(driver, TimeSpan.FromSeconds(200));
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//a[@href='#/accounts/edit?return_module=Accounts&return_action=DetailView']")));
-            accountsClassPageObjects.CreateAccounts.Click();
-            
-            
-            //View account
-            WebDriverWait wait3 = new WebDriverWait(driver, TimeSpan.FromSeconds(200));
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//a[normalize-space()='Accounts']")));
-            accountsClassPageObjects.counts.Click();
-            WebDriverWait wait4 = new WebDriverWait(driver, TimeSpan.FromSeconds(200));
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//a[@href='#/accounts/index?return_module=Accounts&return_action=DetailView']")));
-            accountsClassPageObjects.View.Click();
-            
-
-            //Create account
-            WebDriverWait wait5 = new WebDriverWait(driver, TimeSpan.FromSeconds(200));
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//input[@class='form-control form-control-sm ng-pristine ng-invalid is-invalid ng-touched']")));
-            accountsClassPageObjects.name.SendKeys(Accountname);
-
-            accountsClassPageObjects.Save.Click();*/
-
-
-
         }
 
     }
